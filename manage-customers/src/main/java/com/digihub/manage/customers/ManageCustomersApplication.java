962d7f04e10c830b12d0600efb247fa1c559dc4e
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-08-24 01:00:16 -0300

    Primeiro commit

---
+++ manage-customers/src/main/java/com/digihub/manage/customers/ManageCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.digihub.manage.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageCustomersApplication.class, args);
+	}
+
+}
---
+++ manage-customers/src/test/java/com/digihub/manage/customers/ManageCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digihub.manage.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
